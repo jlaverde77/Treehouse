17ee6b105cb81e91fc22b6f11c60ff25f3032411
Jose Laverde <jlaverde77@gmail.com>
2016-09-12 01:42:48 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+class Program
+{
+    static void Main()
+    {
+        string userName = "No Name";
+        System.Console.Write("What is your name?");
+        userName = System.Console.ReadLine();
+        System.Console.Write("Your name is " + userName);
+    }
+}
